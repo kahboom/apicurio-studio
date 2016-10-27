7580279c298ddde174ce2a3a1fa27da33e96fe13
kahboom <rachelyordan@gmail.com>
2016-10-27 16:03:32 -0400

    Rm vendor references not needed

--- front-end/app/studio/main.ts
+++
@@ -1,5 +0,0 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {StudioModule}           from './studio.module';
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(StudioModule);
--- front-end/app/studio/vendor.browser.ts
+++ front-end/app/studio/vendor.browser.ts
@@ -31,49 +31,8 @@ require('jquery/dist/jquery.min');
 //Bootstrap JS
 require('bootstrap/dist/js/bootstrap.min');
 
-// Datatables, jQuery Grid Component
-require('datatables/media/js/jquery.dataTables.min');
-require('drmonty-datatables-colvis/js/dataTables.colVis');
-require('datatables.net-colreorder/js/dataTables.colReorder');
-
-// Moment
-require('moment/min/moment.min.js');
-
-// Bootstrap Combobox
-//require('patternfly-bootstrap-combobox/js/bootstrap-combobox');
-
-// Bootstrap Date Picker
-require('bootstrap-datepicker/dist/js/bootstrap-datepicker.min');
-
-// Bootstrap Date Time Picker - requires Moment
-require('eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min');
-
-// Bootstrap Select
-require('bootstrap-select/dist/js/bootstrap-select.min');
-
-// Bootstrap Touchspin
-require('bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min');
-
-// Bootstrap Tree View
-require('patternfly-bootstrap-treeview/dist/bootstrap-treeview.min');
-
-// Google Code Prettify - Syntax highlighting of code snippets
-require('google-code-prettify/bin/prettify.min');
-
-// MatchHeight - Used to make sure dashboard cards are the same height
-require('jquery-match-height/jquery.matchHeight-min');
-
-// Charting
-require('c3');
-require('d3');
-
 // PatternFly Custom Componets -  Sidebar, Popovers and Datatables Customizations
-// Note: jquery.dataTables.js must occur in the html source before patternfly*.js
 require('patternfly/dist/js/patternfly.min.js');
-
-// Angular Application? You May Want to Consider Pulling Angular-PatternFly And Angular-UI Bootstrap instead of
-// bootstrap.js
-// See https://github.com/patternfly/angular-patternfly for more information
 
 if ('production' === ENV) {
     // Production
