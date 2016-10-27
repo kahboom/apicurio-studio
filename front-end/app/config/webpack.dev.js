0a8c37e52ff3b1c1322d240e2147a49e4a25dc18
kahboom <rachelyordan@gmail.com>
2016-10-27 15:39:17 -0400

    Rm unrelated files, add Webpack files

---
+++ front-end/app/config/webpack.dev.js
@@ -0,0 +1,239 @@
+/**
+ * @author: @AngularClass
+ */
+
+const helpers = require('./helpers');
+const webpackMerge = require('webpack-merge'); // used to merge webpack configs
+const commonConfig = require('./webpack.common.js'); // the settings that are common to prod and dev
+
+/**
+ * Webpack Plugins
+ */
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const NamedModulesPlugin = require('webpack/lib/NamedModulesPlugin');
+
+/**
+ * Webpack Constants
+ */
+const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 8080;
+const HMR = helpers.hasProcessFlag('hot');
+const METADATA = webpackMerge(commonConfig.metadata, {
+  host: HOST,
+  port: PORT,
+  ENV: ENV,
+  HMR: HMR
+});
+
+// proxyConfig configuration
+const USE_PROXY = process.env.USE_PROXY;
+var proxyConfig = {};
+
+// Set up variables to pass along to the frontend so it can connect to backend services
+const FABRIC8_FORGE = process.env.FABRIC8_FORGE;
+const KUBERNETES_MASTER = process.env.KUBERNETES_MASTER;
+const K8S_PROVIDER = process.env.K8S_PROVIDER || 'kubernetes';
+const FRONTEND_MODE = process.env.FRONTEND_MODE || 'dev';
+
+var frontendConfig = {
+  urls: {
+
+  },
+  k8sProvider: K8S_PROVIDER,
+  frontendMode: FRONTEND_MODE
+};
+// URL for talking to the fabric8 forge service
+if (FABRIC8_FORGE) {
+  frontendConfig['urls']['FABRIC8_FORGE'] = FABRIC8_FORGE;
+}
+// URL to talk to the kubernetes API server
+if (KUBERNETES_MASTER) {
+  frontendConfig['urls']['KUBERNETES_MASTER'] = KUBERNETES_MASTER;
+}
+
+// Set up the backend URLs to either use the proxyConfig or not
+if (USE_PROXY === 'true' && FABRIC8_FORGE) {
+  console.log('Enabling fabric8 forge proxyConfig');
+  proxyConfig['/forge'] = {
+    target: FABRIC8_FORGE + '/api/',
+    secure: false
+  }
+  frontendConfig['urls']['FABRIC8_FORGE'] = '/forge';
+}
+if (USE_PROXY === 'true' && KUBERNETES_MASTER) {
+  console.log('Enabling kubernetes API server proxyConfig');
+  var k8sConfig = proxyConfig['/k8s'] = {
+    target: KUBERNETES_MASTER,
+    secure: false
+  }
+  if (KUBERNETES_MASTER.indexOf('/k8s') > 0) {
+    k8sConfig.pathRewrite = {
+      '^/k8s/': '/'
+    };
+  }
+  frontendConfig['urls']['KUBERNETES_MASTER'] = '/k8s';
+}
+
+console.log("Using frontend configuration: ", frontendConfig);
+console.log("Using proxyConfig configuration: ", proxyConfig);
+console.log("Building to ", METADATA.dist);
+
+
+/**
+ * Webpack configuration
+ *
+ * See: http://webpack.github.io/docs/configuration.html#cli
+ */
+module.exports = webpackMerge(commonConfig, {
+
+  /**
+   * Merged metadata from webpack.common.js for index.html
+   *
+   * See: (custom attribute)
+   */
+  metadata: METADATA,
+
+  /**
+   * Switch loaders to debug mode.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#debug
+   */
+  debug: true,
+
+  /**
+   * Developer tool to enhance debugging
+   *
+   * See: http://webpack.github.io/docs/configuration.html#devtool
+   * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
+   */
+  devtool: 'cheap-module-source-map',
+
+  /**
+   * Options affecting the output of the compilation.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#output
+   */
+  output: {
+
+    /**
+     * The output directory as absolute path (required).
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-path
+     */
+    path: helpers.root(METADATA.dist),
+
+    /**
+     * Specifies the name of each output file on disk.
+     * IMPORTANT: You must not specify an absolute path here!
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-filename
+     */
+    filename: '[name].bundle.js',
+
+    /**
+     * The filename of the SourceMaps for the JavaScript files.
+     * They are inside the output.path directory.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
+     */
+    sourceMapFilename: '[name].map',
+
+    /** The filename of non-entry chunks as relative path
+     * inside the output.path directory.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
+     */
+    chunkFilename: '[id].chunk.js',
+
+    library: 'ac_[name]',
+    libraryTarget: 'var'
+  },
+
+  plugins: [
+
+    /**
+     * Plugin: DefinePlugin
+     * Description: Define free variables.
+     * Useful for having development builds with debug logging or adding global constants.
+     *
+     * Environment helpers
+     *
+     * See: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+     */
+    // NOTE: when adding more properties, make sure you include them in custom-typings.d.ts
+    new DefinePlugin({
+      'ENV': JSON.stringify(METADATA.ENV),
+      'HMR': METADATA.HMR,
+      'process.env': {
+        'ENV': JSON.stringify(METADATA.ENV),
+        'NODE_ENV': JSON.stringify(METADATA.ENV),
+        'HMR': METADATA.HMR
+      }
+    }),
+
+    /**
+       * Plugin: NamedModulesPlugin (experimental)
+       * Description: Uses file names as module name.
+       *
+       * See: https://github.com/webpack/webpack/commit/a04ffb928365b19feb75087c63f13cadfc08e1eb
+       */
+      new NamedModulesPlugin()
+
+  ],
+
+  /**
+   * Static analysis linter for TypeScript advanced options configuration
+   * Description: An extensible linter for the TypeScript language.
+   *
+   * See: https://github.com/wbuchwalter/tslint-loader
+   */
+
+  tslint: {
+    emitErrors: false,
+    failOnHint: false,
+    resourcePath: 'src'
+  },
+
+  /**
+   * Webpack Development Server configuration
+   * Description: The webpack-dev-server is a little node.js Express server.
+   * The server emits information about the compilation state to the client,
+   * which reacts to those events.
+   *
+   * See: https://webpack.github.io/docs/webpack-dev-server.html
+   */
+  devServer: {
+    port: METADATA.port,
+    host: METADATA.host,
+    historyApiFallback: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    },
+    proxy: proxyConfig,
+    outputPath: helpers.root(METADATA.dist),
+    setup: function(app) {
+      app.get('/config.json', function(req, res) {
+        res.json(frontendConfig);
+      });
+
+    }
+  },
+
+  /*
+   * Include polyfills or mocks for various node stuff
+   * Description: Node configuration
+   *
+   * See: https://webpack.github.io/docs/configuration.html#node
+   */
+
+  node: {
+    global: 'window',
+    crypto: 'empty',
+    process: true,
+    module: false,
+    clearImmediate: false,
+    setImmediate: false
+  }
+});
---
+++ front-end/app/studio/vendor.browser.ts
@@ -0,0 +1,85 @@
+// For vendors for example jQuery, Lodash, angular2-jwt just import them here unless you plan on
+// chunking vendors files for async loading. You would need to import the async loaded vendors
+// at the entry point of the async loaded file. Also see custom-typings.d.ts as you also need to
+// run `typings install x` where `x` is your module
+
+// TODO(gdi2290): switch to DLLs
+
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/router';
+
+// AngularClass
+import '@angularclass/hmr';
+
+
+// RxJS
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+
+
+// PatternFly JS Dependencies Entry
+
+// jQuery
+require('jquery/dist/jquery.min');
+
+//Bootstrap JS
+require('bootstrap/dist/js/bootstrap.min');
+
+// Datatables, jQuery Grid Component
+require('datatables/media/js/jquery.dataTables.min');
+require('drmonty-datatables-colvis/js/dataTables.colVis');
+require('datatables.net-colreorder/js/dataTables.colReorder');
+
+// Moment
+require('moment/min/moment.min.js');
+
+// Bootstrap Combobox
+//require('patternfly-bootstrap-combobox/js/bootstrap-combobox');
+
+// Bootstrap Date Picker
+require('bootstrap-datepicker/dist/js/bootstrap-datepicker.min');
+
+// Bootstrap Date Time Picker - requires Moment
+require('eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min');
+
+// Bootstrap Select
+require('bootstrap-select/dist/js/bootstrap-select.min');
+
+// Bootstrap Touchspin
+require('bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min');
+
+// Bootstrap Tree View
+require('patternfly-bootstrap-treeview/dist/bootstrap-treeview.min');
+
+// Google Code Prettify - Syntax highlighting of code snippets
+require('google-code-prettify/bin/prettify.min');
+
+// MatchHeight - Used to make sure dashboard cards are the same height
+require('jquery-match-height/jquery.matchHeight-min');
+
+// Charting
+require('c3');
+require('d3');
+
+// PatternFly Custom Componets -  Sidebar, Popovers and Datatables Customizations
+// Note: jquery.dataTables.js must occur in the html source before patternfly*.js
+require('patternfly/dist/js/patternfly.min.js');
+
+// Angular Application? You May Want to Consider Pulling Angular-PatternFly And Angular-UI Bootstrap instead of
+// bootstrap.js
+// See https://github.com/patternfly/angular-patternfly for more information
+
+if ('production' === ENV) {
+    // Production
+    
+    
+} else {
+    // Development
+    
+}
